6b432a59712eef0389cdeb2b427ef6d35add04e3
BrickheadJohnny <johnny.zgen@gmail.com>
2021-11-09 10:34:42 +0100

    Auth page UI

--- src/pages/auth/[serverId]/[userId].tsx
+++ src/pages/auth/[serverId]/[userId].tsx
@@ -1,9 +1,18 @@
-import { useColorMode } from "@chakra-ui/react"
+import {
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  useColorMode,
+  VStack,
+} from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
 import Card from "components/common/Card"
 import ConnectWalletAlert from "components/common/ConnectWalletAlert"
 import Layout from "components/common/Layout"
 import { useRouter } from "next/router"
+import { DiscordLogo } from "phosphor-react"
 import { useEffect } from "react"
 
 const Page = (): JSX.Element => {
@@ -17,12 +26,7 @@ const Page = (): JSX.Element => {
   }, [])
 
   return (
-    <Layout
-      title="Discord authentication"
-      description=""
-      imageUrl="/dc-logo.svg"
-      imageBg="DISCORD.500"
-    >
+    <Layout title="Authentication">
       {account ? (
         <>
           {/* <Alert status="error" mb="6" pb="5">
@@ -33,8 +37,25 @@ const Page = (): JSX.Element => {
               </AlertDescription>
             </Stack>
           </Alert> */}
-          <Card p={{ base: 5, md: 7 }} mx="auto" maxW="lg">
-            DC auth
+          <Card px={{ base: 5, md: 7 }} py={{ base: 7, md: 9 }} mx="auto" maxW="md">
+            <VStack width="full" alignItems="start" spacing={4}>
+              <Heading as="h2" mb={2} fontFamily="display" fontSize="xl">
+                Connect with Discord
+              </Heading>
+              <Text>
+                In order to join every guild on this Discord server, please connect
+                with your Discord account.
+              </Text>
+              <Flex alignItems="center" pt={8} width="full">
+                <Button
+                  mx="auto"
+                  colorScheme="DISCORD"
+                  leftIcon={<Icon as={DiscordLogo} />}
+                >
+                  Authenticate with Discord
+                </Button>
+              </Flex>
+            </VStack>
           </Card>
         </>
       ) : (
