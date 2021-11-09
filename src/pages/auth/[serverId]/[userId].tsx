20f191a52416254dedfdb276f77f2dfbb4ea6fc2
BrickheadJohnny <johnny.zgen@gmail.com>
2021-11-09 10:20:10 +0100

    Basic account error handling

--- src/pages/auth/[serverId]/[userId].tsx
+++ src/pages/auth/[serverId]/[userId].tsx
@@ -1,5 +1,7 @@
 import { useColorMode } from "@chakra-ui/react"
+import { useWeb3React } from "@web3-react/core"
 import Card from "components/common/Card"
+import ConnectWalletAlert from "components/common/ConnectWalletAlert"
 import Layout from "components/common/Layout"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
@@ -8,6 +10,7 @@ const Page = (): JSX.Element => {
   // Setting up the dark mode, because this is a "static" page
   const { setColorMode } = useColorMode()
   const router = useRouter()
+  const { account } = useWeb3React()
 
   useEffect(() => {
     setColorMode("dark")
@@ -20,9 +23,23 @@ const Page = (): JSX.Element => {
       imageUrl="/dc-logo.svg"
       imageBg="DISCORD.500"
     >
-      <Card p={{ base: 5, md: 7 }} mx="auto" maxW="lg">
-        DC auth
-      </Card>
+      {account ? (
+        <>
+          {/* <Alert status="error" mb="6" pb="5">
+            <AlertIcon />
+            <Stack>
+              <AlertDescription position="relative" top={1} fontWeight="semibold">
+                Invalid server ID or user ID!
+              </AlertDescription>
+            </Stack>
+          </Alert> */}
+          <Card p={{ base: 5, md: 7 }} mx="auto" maxW="lg">
+            DC auth
+          </Card>
+        </>
+      ) : (
+        <ConnectWalletAlert />
+      )}
     </Layout>
   )
 }
